7064115539e91c9f49b4fb9ba0619652f535c6e9
Eliauk <1525281143@qq.com>
2021-06-21 01:24:14 +0800

    r

--- src/main/java/com/controller.java
+++ src/main/java/com/controller.java
@@ -23,9 +23,9 @@ public class controller {
     @RequestMapping("/test")
     public String ss(HttpServletResponse response) throws IOException {
         XSSFWorkbook xssfWorkbook=null;
-
+        try{
 //            FileInputStream file=new FileInputStream("C:\\Users\\nick\\Desktop\\档案接收函.xlsx");
-            ClassPathResource classPathResource=new ClassPathResource("档案接收函.xlsx");
+            ClassPathResource classPathResource=new ClassPathResource("/档案接收函.xlsx");
             InputStream file=classPathResource.getInputStream();
             xssfWorkbook=new XSSFWorkbook(file);
             XSSFSheet sheet=xssfWorkbook.getSheetAt(0);
@@ -34,8 +34,6 @@ public class controller {
             archivesMgrCell.setCellValue("李浩有限公司"+"：");
             XSSFCell nameCell=sheet.getRow(8).getCell(3);
             nameCell.setCellValue("李浩");
-            XSSFCell cell=sheet.getRow(8).getCell(4);
-            System.out.println("c"+cell.getStringCellValue());
 //            CellRangeAddress region = new CellRangeAddress(8, 8, 8, 10);
             XSSFCell cardIdCell=sheet.getRow(8).getCell(7);
             cardIdCell.setCellValue("535464564564564556");
@@ -49,25 +47,39 @@ public class controller {
             nowDateCell.setCellValue("2010年"+"7月"+"5日");
             XSSFCell lastNameCell=sheet.getRow(30).getCell(1);
             lastNameCell.setCellValue("李浩");
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            xssfWorkbook.write(os);
-            byte[] content = os.toByteArray();
-            InputStream is = new ByteArrayInputStream(content);
-            // 设置response参数，可以打开下载页面
-            response.reset();
-            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-            response.addHeader("Content-Disposition", "attachment;filename="+new String("档案接收函".getBytes(),"ISO8859-1") + ".xlsx");
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        xssfWorkbook.write(os);
+        byte[] content = os.toByteArray();
+        InputStream is = new ByteArrayInputStream(content);
+        // 设置response参数，可以打开下载页面
+        response.reset();
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.addHeader("Content-Disposition", "attachment;filename="+new String("档案接收函".getBytes(),"ISO8859-1") + ".xlsx");
 
-            ServletOutputStream sout = response.getOutputStream();
-            OutputStream output;
+        ServletOutputStream sout = response.getOutputStream();
+        BufferedInputStream bis = null;
+        BufferedOutputStream bos = null;
 
-                output = response.getOutputStream();
-                BufferedOutputStream bufferedOutPut = new BufferedOutputStream(output);
-                bufferedOutPut.flush();
-                xssfWorkbook.write(bufferedOutPut);
-                bufferedOutPut.close();
-
-
+        try {
+            bis = new BufferedInputStream(is);
+            bos = new BufferedOutputStream(sout);
+            byte[] buff = new byte[3072];
+            int bytesRead;
+            // Simple read/write loop.
+            while (-1 != (bytesRead = bis.read(buff, 0, buff.length))) {
+                bos.write(buff, 0, bytesRead);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null)
+                bis.close();
+            if (bos != null)
+                bos.close();
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
         return null;
     }
 }
